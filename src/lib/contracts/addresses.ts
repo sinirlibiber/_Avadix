c8ce06dde7339d0641d63bb2f13a47e03f6d5629
gumusbey <98549582+sinirlibiber@users.noreply.github.com>
2026-03-11 02:50:58 +0300

    Update addresses.ts

--- src/lib/contracts/addresses.ts
+++ src/lib/contracts/addresses.ts
@@ -6,7 +6,7 @@ import DAOABI from './AvadixDAO.json'
 import DonationsABI from './AvadixDonations.json'
 
 export const FUJI_ADDRESSES = {
-  PredictionMarket: '0x64eEB71c16072A83F9260D0C5a17f3D53d7B2da3' as `0x${string}`,
+  PredictionMarket: '0x9be459308edCD5BA01941f781d20c1e5F12aACf2' as `0x${string}`,
   AvadixDAO:        '0xa70770942ECba3aBCEB0096a824e94b2FB01fA27' as `0x${string}`,
   AvadixDonations:  '0xC7f1D448570f052aA879326ec3BA60C20005Fcd2' as `0x${string}`,
 }
